10cf7d807a55fb41874db8df43642a63db57d09f
Mako1688 <128003121+Mako1688@users.noreply.github.com>
2025-02-04 17:58:28 -0800

    cleaned up code

--- src/autoHTN.py
+++ src/autoHTN.py
@@ -1,5 +1,3 @@
-from os import stat
-from sys import settrace
 import pyhop
 import json
 
@@ -19,6 +17,7 @@ def produce(state, ID, item):
 pyhop.declare_methods('produce', produce)
 
 def make_method(name, rule):
+    # Create a method for a given recipe
     def method(state, ID):
         subTasks = []  # Initialize the list of subtasks
         for prereq in rule["Recipes"][name]:  # Iterate over the prerequisites of the recipe
@@ -33,49 +32,34 @@ def make_method(name, rule):
     return method  # Return the method
 
 def declare_methods(data):
-    for item in data["Items"]:  # Iterate over the items in the data
-        methods = []  # Initialize the list of methods
-        for recipe in data["Recipes"]:  # Iterate over the recipes in the data
-            newMethod = None  # Initialize the new method
-            for category in data["Recipes"][recipe]:  # Iterate over the categories in the recipe
-                if category == "Produces":  # If the category is a production
-                    if item == next(iter(data["Recipes"][recipe][category])):  # If the item is produced by the recipe
-                        newMethod = make_method(recipe, data)  # Create the method for the recipe
-                        newMethod.__name__ = recipe  # Set the name of the method
-                        if methods == []:  # If the list of methods is empty
-                            methods.append(newMethod)  # Add the new method to the list
-                        else:  # If the list of methods is not empty
-                            for method in methods:  # Iterate over the methods in the list
-                                if (data["Recipes"][method.__name__]["Time"] >= data["Recipes"][recipe]["Time"]) and newMethod not in methods:  # If the new method is faster than the existing method
-                                    methods.insert(methods.index(method), newMethod)  # Insert the new method before the existing method
-                            if newMethod not in methods:  # If the new method is not in the list
+    # Declare methods for all items and tools
+    def declare_for_category(category):
+        for item in data[category]:  # Iterate over the items in the category
+            methods = []  # Initialize the list of methods
+            for recipe in data["Recipes"]:  # Iterate over the recipes in the data
+                newMethod = None  # Initialize the new method
+                for cat in data["Recipes"][recipe]:  # Iterate over the categories in the recipe
+                    if cat == "Produces":  # If the category is a production
+                        if item == next(iter(data["Recipes"][recipe][cat])):  # If the item is produced by the recipe
+                            newMethod = make_method(recipe, data)  # Create the method for the recipe
+                            newMethod.__name__ = recipe  # Set the name of the method
+                            if methods == []:  # If the list of methods is empty
                                 methods.append(newMethod)  # Add the new method to the list
-        name = "produce_" + item  # Create the name for the method
-        pyhop.declare_methods(name, *methods)  # Declare the methods to pyhop
-        methods.clear()  # Clear the list of methods
+                            else:  # If the list of methods is not empty
+                                for method in methods:  # Iterate over the methods in the list
+                                    if (data["Recipes"][method.__name__]["Time"] >= data["Recipes"][recipe]["Time"]) and newMethod not in methods:  # If the new method is faster than the existing method
+                                        methods.insert(methods.index(method), newMethod)  # Insert the new method before the existing method
+                                if newMethod not in methods:  # If the new method is not in the list
+                                    methods.append(newMethod)  # Add the new method to the list
+            name = "produce_" + item  # Create the name for the method
+            pyhop.declare_methods(name, *methods)  # Declare the methods to pyhop
+            methods.clear()  # Clear the list of methods
 
-    for tool in data["Tools"]:  # Iterate over the tools in the data
-        methods = []  # Initialize the list of methods
-        for recipe in data["Recipes"]:  # Iterate over the recipes in the data
-            newMethod = None  # Initialize the new method
-            for category in data["Recipes"][recipe]:  # Iterate over the categories in the recipe
-                if category == "Produces":  # If the category is a production
-                    if tool == next(iter(data["Recipes"][recipe][category])):  # If the tool is produced by the recipe
-                        newMethod = make_method(recipe, data)  # Create the method for the recipe
-                        newMethod.__name__ = recipe  # Set the name of the method
-                        if methods == []:  # If the list of methods is empty
-                            methods.append(newMethod)  # Add the new method to the list
-                        else:  # If the list of methods is not empty
-                            for method in methods:  # Iterate over the methods in the list
-                                if (data["Recipes"][method.__name__]["Time"] >= data["Recipes"][recipe]["Time"]) and newMethod not in methods:  # If the new method is faster than the existing method
-                                    methods.insert(methods.index(method), newMethod)  # Insert the new method before the existing method
-                            if newMethod not in methods:  # If the new method is not in the list
-                                methods.append(newMethod)  # Add the new method to the list
-        name = "produce_" + tool  # Create the name for the method
-        pyhop.declare_methods(name, *methods)  # Declare the methods to pyhop
-        methods.clear()  # Clear the list of methods
+    declare_for_category("Items")  # Declare methods for items
+    declare_for_category("Tools")  # Declare methods for tools
 
 def make_operator(rule):
+    # Create an operator for a given recipe
     def operator(state, ID):
         for element in rule:  # Iterate over the elements in the rule
             if element == 'Produces':  # If the element is a production
@@ -86,27 +70,24 @@ def make_operator(rule):
             elif element == 'Requires':  # If the element is a requirement
                 for item in rule[element]:  # Iterate over the required items
                     value = rule[element][item]  # Get the value of the required item
-                    if getattr(state, item)[ID] >= value:  # If the state has enough of the required item
-                        setattr(state, item, {ID: value})  # Update the state with the required item
-                    else:  # If the state does not have enough of the required item
+                    if getattr(state, item)[ID] < value:  # If the state does not have enough of the required item
                         return False  # Return False
             elif element == 'Consumes':  # If the element is a consumption
                 for item in rule[element]:  # Iterate over the items to be consumed
                     value = rule[element][item]  # Get the value of the item to be consumed
-                    if getattr(state, item)[ID] >= value:  # If the state has enough of the item to be consumed
-                        setattr(state, item, {ID: getattr(state, item)[ID] - value})  # Update the state with the consumed item
-                    else:  # If the state does not have enough of the item to be consumed
+                    if getattr(state, item)[ID] < value:  # If the state does not have enough of the item to be consumed
                         return False  # Return False
+                    setattr(state, item, {ID: getattr(state, item)[ID] - value})  # Update the state with the consumed item
             elif element == "Time":  # If the element is time
                 value = rule[element]  # Get the value of the time
-                if getattr(state, "time")[ID] >= value:  # If the state has enough time
-                    setattr(state, "time", {ID: getattr(state, "time")[ID] - value})  # Update the state with the time
-                else:  # If the state does not have enough time
+                if getattr(state, "time")[ID] < value:  # If the state does not have enough time
                     return False  # Return False
+                setattr(state, "time", {ID: getattr(state, "time")[ID] - value})  # Update the state with the time
         return state  # Return the updated state
     return operator  # Return the operator
 
 def declare_operators(data):
+    # Declare operators for all recipes
     ops = []  # Initialize the list of operators
     for element in data["Recipes"]:  # Iterate over the recipes in the data
         holder = make_operator(data["Recipes"][element])  # Create the operator for the recipe
@@ -115,6 +96,7 @@ def declare_operators(data):
     pyhop.declare_operators(*ops)  # Declare the operators to pyhop
 
 def add_heuristic(data, ID):
+    # Add heuristics for pruning the search space
     def prune_large_quantities(state, curr_task, tasks, plan, depth, calling_stack):
         for item in data["Items"]:  # Iterate over the items in the data
             if item != "rail":  # If the item is not a rail
@@ -137,9 +119,7 @@ def add_heuristic(data, ID):
                     made_check = "made_" + tool  # Create the name for the tool flag
                     if getattr(state, made_check)[ID] != 0:  # If the tool has already been made
                         return True  # Prune the branch
-                    else:  # If the tool has not been made
-                        setattr(state, made_check, {ID: getattr(state, made_check)[ID] + 1})  # Update the tool flag in the state
-                        return False  # Do not prune the branch
+                    setattr(state, made_check, {ID: getattr(state, made_check)[ID] + 1})  # Update the tool flag in the state
         return False  # Do not prune the branch
 
     pyhop.add_check(prune_large_quantities)  # Add the prune_large_quantities heuristic to pyhop
@@ -161,19 +141,24 @@ def set_up_goals(data, ID):
     return [('have_enough', ID, item, num) for item, num in data['Goal'].items()]
 
 if __name__ == '__main__':
-    rules_filename = 'crafting.json'  # Set the filename for the crafting rules
+    rules_filename = 'crafting.json' 
 
-    with open(rules_filename) as f:  # Open the crafting rules file
-        data = json.load(f)  # Load the crafting rules from the file
+    with open(rules_filename) as f: 
+        data = json.load(f)
 
-    state = set_up_state(data, 'agent', time=300)  # Set up the initial state with 300 time units
+    state = set_up_state(data, 'agent', time=250) # Set time
     goals = set_up_goals(data, 'agent')  # Set up the goals
 
-    declare_operators(data)  # Declare the operators
-    declare_methods(data)  # Declare the methods
-    add_heuristic(data, 'agent')  # Add the heuristics
+    declare_operators(data)  
+    declare_methods(data)  
+    add_heuristic(data, 'agent')  
 
-    pyhop.print_operators()  # Print the operators
-    pyhop.print_methods()  # Print the methods
-
-    pyhop.pyhop(state, [('have_enough', 'agent', 'cart', 1), ('have_enough', 'agent', 'rail', 20)], verbose=1)  # Run the planner with the specified goals and verbosity level
+    pyhop.print_operators() 
+    pyhop.print_methods()  
+    
+	# Hint: verbose output can take a long time even if the solution is correct; 
+	# try verbose=1 if it is taking too long
+    # pyhop.pyhop(state, goals, verbose=3)
+    pyhop.pyhop(state, [('have_enough', 'agent', 'wood', 12)], verbose=1)
+    # pyhop.pyhop(state, [('have_enough', 'agent', 'cart', 1),('have_enough', 'agent', 'rail', 20)], verbose=3)
+	# pyhop.pyhop(state, [('have_enough', 'agent', 'cart', 1),('have_enough', 'agent', 'rail', 20)], verbose=3)
